5106487b28064227bbba1a01b41bd14669830ec8
asrani <asrani.27@gmail.com>
2022-08-21 09:23:35 +0800

    f

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Home;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function updateSuhu($value)
+    {
+        Home::first()->update([
+            'suhu' => $value,
+        ]);
+
+        return response()->json('sukses');
+    }
+
+    public function updateTinggiair($value)
+    {
+        Home::first()->update([
+            'tinggi_air' => $value,
+        ]);
+
+        return response()->json('sukses');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 
-Route::get('/update/suhu={id}', [ApiController::class, 'updateSuhu']);
-Route::get('/update/tinggiair={id}', [ApiController::class, 'updateSuhu']);
+Route::get('/update/suhu={value}', [ApiController::class, 'updateSuhu']);
+Route::get('/update/tinggiair={value}', [ApiController::class, 'updateTinggiair']);
